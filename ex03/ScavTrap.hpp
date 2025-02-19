80c1cb25b7d37422fddb5e06f533062a083d8e4c
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2025-02-19 11:56:27 +0100

    ex03 - Add guardGateMode property and update default values for constructors

--- ex03/ScavTrap.hpp
+++ ex03/ScavTrap.hpp
@@ -6,26 +6,35 @@
 /*   By: fcarranz <fcarranz@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 11:12:57 by fcarranz          #+#    #+#             */
-/*   Updated: 2025/02/17 14:30:03 by fcarranz         ###   ########.fr       */
+/*   Updated: 2025/02/19 11:29:46 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
 #include <string>
 #include "ClapTrap.hpp"
 
 class ScavTrap : virtual public ClapTrap {
 
 public:
-	ScavTrap(void);
-	~ScavTrap(void);
+  ScavTrap(void);
+  ScavTrap(std::string name);
+  ScavTrap(const ScavTrap &other);
+  ScavTrap(std::string name, unsigned int hp, unsigned int ep, unsigned int ad);
 
-	ScavTrap(std::string name);
+  ScavTrap &operator=(const ScavTrap &other);
 
-	ScavTrap(const ScavTrap &other);
-	ScavTrap &operator=(const ScavTrap &other);
+  ~ScavTrap(void);
 
-	void guardGate(void);
-	void attack(const std::string& target);
+  void attack(const std::string& target);
+  void guardGate(void);
+
+protected:
+  static const std::string _defName;
+  static const unsigned int _defHP;
+  static const unsigned int _defEP;
+  static const unsigned int _defAD;
+
+private:
+  bool guardGateMode;
 };
